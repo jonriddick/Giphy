11fefa4b610ed831978c62204d18763af8939769
jonriddick <jonathansyerriddick@gmail.com>
2017-07-30 20:44:40 -0400

    V1.4 Yeah buddy!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,115 @@
- //build array of footballers
-var ballers = ["Matt Ryan", "Julio Jones"]
+//build array of footballers
+var ballers = ["Matt Ryan", "Julio Jones", "Aaron Rodgers", "Von Miller", "Vic Beasley", "Russell Wilson", "Devonta Freeman", "Deion Sanders"];
 
 //function to create buttons
 
 function renderButtons (){
-	$("#buttons").empty();
+     $("#buttons").empty();
 
-	for (var i = 0; i < ballers.length; i++) {
+     for (var i = 0; i < ballers.length; i++) {
 
           // Then dynamicaly generating buttons for each movie in the array.
           // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
           var a = $("<button>");
           // Adding a class
-          a.addClass("movie");
+          a.addClass("baller");
+          a.addClass("btn");
           // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", movies[i]);
+          a.attr("data-name", ballers[i]);
           // Providing the button's text with a value of the movie at index i
-          a.text(movies[i]);
+          a.text(ballers[i]);
           // Adding the button to the HTML
           $("#buttons").append(a);
         }
 }
 
-//build button that adds to ballers array
+// This function handles events where one button is clicked
+      $("#addBaller").on("click", function(event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
 
-var newBaller = 
-ballers.push()
+        // This line will grab the text from the input box
+        var hmm = $("#ballerInput").val().trim();
+        // The movie from the textbox is then added to our array
+        ballers.push(hmm);
+
+        // calling renderButtons which handles the processing of our movie array
+        renderButtons();
+      });
+
+      // Calling the renderButtons function at least once to display the initial list of movies
+      renderButtons();
+
+//
+//Part 2
+//
+
+$(document).on("click", ".baller", function() {
+      $("#giphy").empty();
+      // In this case, the "this" keyword refers to the button that was clicked
+      var player = $(this).attr("data-name");
+
+      // Constructing a URL to search Giphy for the name of the person who said the quote
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        player + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      // Performing our AJAX GET request
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        // After the data comes back from the API
+        .done(function(response) {
+          // Storing an array of results in the results variable
+          var results = response.data;
+
+          // Looping over every result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+              personImage.addClass("pause");
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#giphy").prepend(gifDiv);
+            }
+          }
+        });
+    });
+
+
+//Pause Giphys
+$(document).on("click", ".pause", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
